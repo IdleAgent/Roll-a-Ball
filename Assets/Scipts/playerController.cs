ef92c5f8636df2c056688a00bcd2227e570317ea
Prit Deshbhratar <preetdeshbhratar34@gmail.com>
2026-01-14 00:02:22 +0530

    Initial commit - Roll A BAll Unity project

---
+++ Assets/Scipts/playerController.cs
@@ -0,0 +1,68 @@
+using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX,0.0f,movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 10)
+        {
+            winTextObject.SetActive (true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose";
+        }
+    }
+
+}
